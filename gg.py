fb7a21dc5e2c8df9963fef1b9b7fbe123a6367a7
omadridi858 <84738754+omadridi858@users.noreply.github.com>
2023-09-27 11:35:27 +0200

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,1 +1,20 @@
-print("gg")
+import nmap
+
+# Create an Nmap scanner object
+nm = nmap.PortScanner()
+
+# Define the target host or IP address
+target = "101.188.67.134"
+
+# Perform a basic scan of common ports
+nm.scan(target, arguments="-F")
+
+# Iterate through the scan results and print open ports
+for host, scan_result in nm.all_hosts().items():
+    print(f"Host: {host}")
+    for port, port_info in scan_result["tcp"].items():
+        if port_info["state"] == "open":
+            print(f"Port: {port}, State: {port_info['state']}, Service: {port_info['name']}")
+
+# Print the raw Nmap command used for the scan
+print(f"Nmap command used: {nm.command_line()}")
